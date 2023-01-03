71aa18b488a1f8bcaa1d3952cc6d9938d2485de4
stardvst <lilia.manukyan@hotmail.com>
2023-01-03 21:59:31 +0400

    interacting with capacitor to take a photo

---
+++ src/app/photo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  constructor() {}
+
+  async takePhoto() {
+    await this.getLocation();
+    await Camera.getPhoto({
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+  }
+
+  private async getLocation() {
+    const location = await Geolocation.getCurrentPosition();
+    return location.coords;
+  }
+}
